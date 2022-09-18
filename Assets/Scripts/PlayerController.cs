4a4a955bc71dc7b0551fcd53cc908b091db403ac
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2022-09-17 22:36:59 -0400

    Oops
    I give up in animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     Vector3 Vec;
     public float speed = 15;
     Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
